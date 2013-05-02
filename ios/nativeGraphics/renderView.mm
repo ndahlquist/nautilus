b8d9904d136d507e40f7a3ab338afd72b066f45e
Ling-Ling Zhang <sekishi.kei.ou@gmail.com>
2013-05-02 13:10:11 -0700

    Forgot to add new file

---
+++ ios/nativeGraphics/renderView.mm
@@ -0,0 +1,93 @@
+//
+//  renderView.m
+//  nativeGraphics
+//
+//  Created by Ling-Ling Zhang on 4/30/13.
+//  Copyright (c) 2013 Ling-Ling Zhang. All rights reserved.
+//
+
+#import "renderView.h"
+#import "common.h"
+
+@interface renderView()
+@end
+
+@implementation renderView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _eaglLayer = (CAEAGLLayer *)[self layer];
+        
+        [self setupLayer];
+        [self setupContext];
+        [self setupRenderBuffer];
+        [self setupFrameBuffer];
+        [self setupDisplayLink];
+
+        // Initialization code
+        Setup(self.bounds.size.width, self.bounds.size.height);
+    }
+    return self;
+}
+
++ (Class) layerClass
+{
+    return [CAEAGLLayer class];
+}
+
+- (void)setupLayer {
+    _eaglLayer = (CAEAGLLayer*) self.layer;
+    _eaglLayer.opaque = YES;
+}
+
+- (void)setupContext {
+    EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
+    _context = [[EAGLContext alloc] initWithAPI:api];
+    if (!_context) {
+        NSLog(@"Failed to initialize OpenGLES 2.0 context");
+        exit(1);
+    }
+    
+    if (![EAGLContext setCurrentContext:_context]) {
+        NSLog(@"Failed to set current OpenGL context");
+        exit(1);
+    }
+}
+
+- (void)setupRenderBuffer {
+    glGenRenderbuffers(1, &_colorRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+    [_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
+}
+
+- (void)setupFrameBuffer {
+    GLuint framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                              GL_RENDERBUFFER, _colorRenderBuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depthRenderBuffer);
+}
+
+- (void)setupDisplayLink {
+    CADisplayLink* displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(render:)];
+    [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+}
+
+- (void)render:(CADisplayLink*)displayLink
+{
+    RenderFrame();
+    [_context presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+/*- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
