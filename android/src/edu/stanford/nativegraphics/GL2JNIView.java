1939acb486db7199520211dee23855f9846524ce
Nic Dahlquist <nicd@cs.stanford.edu>
2013-05-05 18:23:11 -0700

    Renamed MainActivity.java. Fixed a memory leak.

--- android/src/edu/stanford/nativegraphics/GL2JNIView.java
+++ android/src/edu/stanford/nativegraphics/GL2JNIView.java
@@ -16,22 +16,6 @@
 
 package edu.stanford.nativegraphics;
 
-/*
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
@@ -336,9 +320,9 @@ class GL2JNIView extends GLSurfaceView {
             if(++lastMeterFrame >= 20) {
 			    float FramesPerSecond = lastMeterFrame / ((System.currentTimeMillis() - lastMeterTime) / 1000.0f);
 			    Message msg = new Message();
-			    msg.arg1 = GL2JNIActivity.overlayUpdater.FPS_UPDATE;
+			    msg.arg1 = MainActivity.overlayUpdater.FPS_UPDATE;
 			    msg.arg2 = (int) FramesPerSecond;
-			    GL2JNIActivity.overlayUpdater.sendMessage(msg);
+			    MainActivity.overlayUpdater.sendMessage(msg);
 			    lastMeterTime = System.currentTimeMillis();
 			    lastMeterFrame = 0;
 		    }
--- android/src/edu/stanford/nativegraphics/MainActivity.java
+++ android/src/edu/stanford/nativegraphics/MainActivity.java
@@ -16,20 +16,22 @@
 
 package edu.stanford.nativegraphics;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+public class MainActivity extends Activity {
 
-public class GL2JNIActivity extends Activity {
-
+	private static String TAG = "GL2JNIActivity";
+	
     public static GL2JNIView mView;
 	public static OverlayUpdater overlayUpdater;
+	public static TextView FPSmeter;
 
     @Override protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -38,6 +40,7 @@ public class GL2JNIActivity extends Activity {
         
         View overlay = getLayoutInflater().inflate(R.layout.overlay, null);
 		addContentView(overlay, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+		FPSmeter = (TextView)findViewById(R.id.FPS_meter);
 		overlayUpdater = new OverlayUpdater();
     }
 
@@ -51,18 +54,17 @@ public class GL2JNIActivity extends Activity {
         mView.onResume();
     }
     
-    @SuppressLint("HandlerLeak") // TODO: Fix leak condition
-	class OverlayUpdater extends Handler {
+	static class OverlayUpdater extends Handler {
 		public final int FPS_UPDATE = 0;
 
 		@Override
 		public void handleMessage(Message msg) {
 			switch(msg.arg1) {
 			case FPS_UPDATE:
-				TextView FPSmeter = (TextView)findViewById(R.id.FPS_meter);
 				FPSmeter.setText(msg.arg2 + " FPS");
 				return;
 			default:
+				Log.e(TAG, "Unrecognized message " + msg.arg1);
 				return;
 			}
 		}
