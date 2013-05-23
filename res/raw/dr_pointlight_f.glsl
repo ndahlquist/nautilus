804df426ed1115d3e839b0347c8acbe807f7b98b
Nic Dahlquist <nicd@stanford.edu>
2013-05-22 20:50:43 -0700

    Fixed matrices for light projection.

--- res/raw/dr_pointlight_f.glsl
+++ res/raw/dr_pointlight_f.glsl
@@ -7,22 +7,21 @@ varying vec3 v_Position;		// Interpolated position for this fragment.
 varying vec3 v_Normal;         	// Interpolated normal for this fragment.
 varying vec2 v_TexCoordinate;   // Interpolated texture coordinate per fragment.
 
-uniform vec3 u_LightPos;
 uniform int u_FragWidth;
 uniform int u_FragHeight;
 
 void main() {
 
-    vec3 LightPos = vec3(u_MVMatrix * vec4(u_LightPos, 0.0));
+    vec3 LightPos = vec3(u_MVMatrix * vec4(0.0, 0.0, 0.0, 1.0));
 
-    vec3 normpos = texture2D(u_Texture, vec2(gl_FragCoord.x / float(u_FragWidth), gl_FragCoord.y / float(u_FragHeight))).rgb;
-    vec3 pos = vec3(normpos.x*10.0, normpos.y*10.0, normpos.z*100.0 - 150.0); // Reverse the mapping function.
+    vec3 mappos = texture2D(u_Texture, vec2(gl_FragCoord.x / float(u_FragWidth), gl_FragCoord.y / float(u_FragHeight))).rgb;
+    vec3 pos = vec3(mappos.x*10.0, mappos.y*10.0 * 5.0, mappos.z*100.0 - 150.0); // Reverse the mapping function. // TODO: 5 multiplier
 	
 	vec3 delta = LightPos - pos;
 	
 	float distsq = delta.x * delta.x + delta.y * delta.y + delta.z * delta.z;
 
-    float val = 800.0 / distsq;
+    float val = 20.0 / distsq;
 
 	gl_FragColor = vec4(val, val, val, 1.0);
 }
