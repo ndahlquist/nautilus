e87151f049654aec5b2fcb406c970d8c54addcda
Nic Dahlquist <nicd@stanford.edu>
2013-06-03 14:50:49 -0700

    Builds on linux.

--- common/Fluid.h
+++ common/Fluid.h
@@ -547,7 +547,7 @@ void Fluid::ApplyPressure()
     //std::cout<<b<<endl;
     //std::cout<<mat;
     // Solving:
-    Eigen::ConjugateGradient<Eigen::SparseMatrix<double>> solver(mat);
+    Eigen::ConjugateGradient<Eigen::SparseMatrix<double> > solver(mat);
     Eigen::VectorXd x = solver.solve(b); // use the factorization to solve for the given right hand side
     
     //update pressure
